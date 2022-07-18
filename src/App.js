1b1d0af62b0ce65e2c11476f33f8f37ce9d945c7
Pawid-Priyanto <pawidanto27@gmail.com>
2022-07-19 00:01:14 +0700

    complex state condotional rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import './App.css'
 const App = () => {
-  // const [left, setLeft] = useState(0)
-  // const [right, setRight] = useState(0)
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  // const [clicks, setClicks] = useState({
+  //   left: 0,
+  //   right: 0
+  // })
+  const [allClicks, setAll] = useState([])
 
   const handleClickLeft = () => {
     // cara 1
@@ -21,7 +22,10 @@ const App = () => {
       //   ...clicks, left: clicks.left + 1
       // }
       // setClicks(newClicks)
-      setClicks({...clicks, left: clicks.left + 1})
+      // setClicks({...clicks, left: clicks.left + 1})
+      // setAll(allClicks.concat('L'))
+      allClicks.push('L')
+      setLeft(left + 1)
   }
 
   const handleClickRight = () => {
@@ -38,22 +42,45 @@ const App = () => {
     // }
     // setClicks(newClicks)
 
-    setClicks({...clicks, right: clicks.right + 1})
+    // setClicks({...clicks, right: clicks.right + 1})
+    setAll(allClicks.concat('R'))
+    setRight(right+1)
   }
 
   return(
     <div className="App-header">
       <div className='box-btn'>
-      {clicks.left}
-      <button className='btn-app' onClick={handleClickLeft}>
-        left
-      </button>
-      <button className='btn-app' onClick={handleClickRight}>
-        right
-      </button>
-      {clicks.right}
+      {left}
+      <Button text="left" handleClick={handleClickLeft}/>
+      <Button text="right" handleClick={handleClickRight}/>
+      {right}
       </div>
+      <History allClicks={allClicks} className="bottom"/>
     </div>
+  )
+}
+
+const History = (props) => {
+  if(props.allClicks.length === 0){
+    return (
+      <div>
+         the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return(
+    <div>
+      button pressed history:{props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return(
+  <button onClick={handleClick} className="btn-app">
+    {text}
+  </button>
+
   )
 }
 
